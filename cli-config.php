f29569e7f33c55703c91b57613f0e658d44618e5
tuken80 <romain.duquesne.mail@gmail.com>
2017-02-21 09:19:14 +0100

    modification cli-config.php

--- cli-config.php
+++ cli-config.php
@@ -1,11 +1,10 @@
 <?php
+
 use Doctrine\ORM\Tools\Console\ConsoleRunner;
 
 // replace with file to your own project bootstrap
 $app = require __DIR__.'/src/app.php';
 require __DIR__.'/config/dev.php';
-
-$app->boot();
 
 // replace with mechanism to retrieve EntityManager in your app
 $entityManager = $app['db.em'];
